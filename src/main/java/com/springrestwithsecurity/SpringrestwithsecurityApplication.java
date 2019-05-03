7d801351e6897c3955c49f8651cf8c76a2382fdb
gulshankumar32 <gulshan.kumar9236@gmail.com>
2019-05-03 13:44:44 +0500

    Initial commit

---
+++ src/main/java/com/springrestwithsecurity/SpringrestwithsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springrestwithsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestwithsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrestwithsecurityApplication.class, args);
+    }
+
+}
